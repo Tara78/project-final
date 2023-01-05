6f70f65f03b0ec460a4347f795614fe5326a5984
Tara78 <70331037+Tara78@users.noreply.github.com>
2023-01-05 16:43:49 +0100

    Contact page/Home/Booking

---
+++ frontend/src/components/pages/Contact.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Contact = () => {
+  return (
+    <div>
+      <Hero>
+        <HeroBackgroundImg></HeroBackgroundImg>
+        <HeroTitle>Contact</HeroTitle>
+        <HeroSubText>
+          Les rdv se prennent de vive voix au 06 23 16 65 75 ou en ligne.
+        </HeroSubText>
+        <HeroText>
+          Pour les commandes de livres dédicacés et tout autre sujet,
+          envoyez-moi un message via le formulaire ci-dessous.
+        </HeroText>
+      </Hero>
+
+      <WrapperSecondPart>
+        <SectionForm>
+
+        </SectionForm>
+        <SectionContactInfo>
+
+        </SectionContactInfo>
+
+      </WrapperSecondPart>
+    </div>
+  );
+}
+
+export default Contact
+const Hero= styled.div`
+  
+`
+const HeroBackgroundImg= styled.div`
+  
+`
+
+const HeroTitle= styled.h1`
+  
+`
+const HeroSubText= styled.h5`
+  
+`
+
+const HeroText = styled.p`
+`;
+
+const WrapperSecondPart = styled.div`
+  width: 95%;
+  height: 100px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+  column-gap: 2.5rem;
+  background-color: #435aa6;
+  
+`;
+
+const SectionForm = styled.div`
+  height: auto;
+  margin-bottom: 1rem;
+  font-size: 25px;
+  cursor: pointer;
+  border-radius: 10px;
+  background-color: #e6e8ec;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`;
+
+const SectionContactInfo = styled.div`
+  height: auto;
+  margin-bottom: 1rem;
+  font-size: 25px;
+  cursor: pointer;
+  border-radius: 10px;
+  background-color: #e6e8ec;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`;
