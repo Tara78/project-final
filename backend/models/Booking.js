092222849bc00f60791a32f3592659b2f0cc15fb
Tara78 <70331037+Tara78@users.noreply.github.com>
2023-01-10 19:53:45 +0100

    booking

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,28 +1,23 @@
 import mongoose from "mongoose";
 
 const BookingSchema = new mongoose.Schema({
-  startTime: {
-    type: Date, // new Date().toISOString(); '2022-12-20T16:41:18.307Z'
-    default:() => new Date(),
+  month: {
+    type: Number,
     required: true,
   },
-  endTime: {
-    type: Date, // new Date().toISOString(); '2022-12-21T21:41:18.307Z'
+  day: {
+    type: Number,
     required: true,
+  },
+  time: {
+    type: Number,
+    required: true,
+  },
+  user:{
+   type: String
+  
   }
-})
-
-// Frontend
-// Fetch Bookings => [{ startTime, endTime }];
-
-// Calendar React Compoent
-// new Date(stateTime)
-
-// 2022-01-31 23:00 => 2022-02-01 01:00
-// month: 1
-// day: 31
-// time: 23:00
-
+});
 
  
 export default mongoose.model("Booking", BookingSchema);
