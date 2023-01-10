8a40b42f663d05c30662f78d577d660fcd3b4e3d
Tara78 <70331037+Tara78@users.noreply.github.com>
2023-01-10 19:53:19 +0100

    moduel

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -8,9 +8,14 @@ import {
 
 const router = express.Router();
 
-router.get("/calendar", userAuthMiddleFunction, async (req, res) => {
+router.get("/free/:month/:day", userAuthMiddleFunction, async (req, res) => {
   try {
-    const calendar = await Calendar.find({});
+    const calendar = await Booking.find({
+      month:req.params.month,
+      day:req.params.day    
+    });
+    // todo filter for NotBooked times
+
     res.status(200).json({
       success: true,
       result: calendar,
@@ -29,6 +34,34 @@ router.delete("/calendar", adminAuthMiddleFunction, async (req, res) => {
     });
   } catch (err) {
     res.status(400).json({ success: false, response: err });
+  }
+});
+
+router.post("/create/:year/:month", adminAuthMiddleFunction,
+async (req, res) => {
+  try {
+    
+   for (let i = 0; i <= new Date(year, month, 0).getDate(); i++) 
+{    
+    const date = new Date(year, month, i);
+
+    if (date.getDay() == 6)
+    {
+      [14,15,16,17,18].forEach((time) => {
+
+       const newBooking= new Booking({
+        year,
+        month, 
+        day: date.getDay(), 
+        time
+       }).save();
+
+      })
+    } 
+  }
+  }
+  catch(err) {
+
   }
 });
 
