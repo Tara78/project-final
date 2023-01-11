4f7c45a440c9925a1419e1df95e589907b97aea0
Tara78 <70331037+Tara78@users.noreply.github.com>
2023-01-11 12:59:35 +0100

    login update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector, batch } from "react-redux";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { API_URL } from "./../utils/utils";
 import user from "./../reducers/user";
 import styled from "styled-components";
-
+import img from "./../img/therapist.jpg";
 
 const Login = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   // Mode varable would be login or register - depend on endpoint (loging or register)- Put default to login
   const [mode, setMode] = useState("login");
@@ -21,9 +22,7 @@ const Login = () => {
     if (accessToken) {
       navigate("/");
     }
-  },
-   [accessToken]
-   );
+  }, [accessToken]);
 
   const onFormSubmit = (e) => {
     e.preventDefault();
@@ -32,7 +31,7 @@ const Login = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name, password }),
+      body: JSON.stringify({ name, password, email }),
     };
     fetch(API_URL(mode), options)
       .then((response) => response.json())
@@ -41,6 +40,7 @@ const Login = () => {
           batch(() => {
             // Dispatch excuting the Action
             dispatch(user.actions.setName(data.response.name));
+            dispatch(user.actions.setEmail(data.response.email));
             dispatch(user.actions.setId(data.response.id));
             dispatch(user.actions.setAccessToken(data.response.accessToken));
             dispatch(user.actions.setError(null));
@@ -49,6 +49,7 @@ const Login = () => {
           batch(() => {
             dispatch(user.actions.setName(null));
             dispatch(user.actions.setId(null));
+            dispatch(user.actions.setEmail(null));
             dispatch(user.actions.setAccessToken(null));
             dispatch(user.actions.setError(data.response));
           });
@@ -57,7 +58,7 @@ const Login = () => {
   };
   return (
     <MainWrapper>
-
+      <HeroBackgroundImg></HeroBackgroundImg>
       <FormWrapper>
         <Section>
           <Label htmlFor="register">
@@ -80,23 +81,26 @@ const Login = () => {
           </Label>
         </Section>
         <Form onSubmit={onFormSubmit}>
-          <Label htmlFor="name">
-            <LabelText> Username</LabelText>
-          </Label>
           <Input
             type="text"
+            placeholder="Username"
             id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          <Label htmlFor="password">
-            <LabelText>Password</LabelText>
-          </Label>
           <Input
             type="password"
+            placeholder="Password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+          />
+          <Input
+            type="email"
+            placeholder="emailadress"
+            id="name"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <Button type="submit">Submit</Button>
           <TextError>
@@ -110,28 +114,41 @@ const Login = () => {
 
 export default Login;
 
-
 /**Styling  */
 const MainWrapper = styled.div`
   width: 100vw;
+  height: auto;
   display: flex;
   justify-content: center;
- 
+  position: relative;
+`;
+const HeroBackgroundImg = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: fixed;
+  /* filter: blur(1px); */
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  /* background-size: contain; */
+  background-size: cover;
+  background-position: center;
 `;
 
 const FormWrapper = styled.div`
   width: 45%;
-  height: 25%;
+  height: auto;
   margin-top: 5%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #d0d4d6;
+  background-color: #bfd6e1;
   font-family: "Inconsolata";
   font-size: 18px;
   border-radius: 8px;
+  z-index: 28;
+  position: absolute;
+  top: 0;
 `;
-
 
 const Label = styled.label`
   display: flex;
@@ -142,7 +159,6 @@ const Label = styled.label`
     border: 1px solid #000;
     line-height: 20px;
     padding: 5px;
-   
   }
 `;
 
@@ -159,8 +175,7 @@ const LabelText = styled.p`
   margin-right: 10px;
   width: 80px;
   text-align: right;
-  color:hsl(0deg 0% 13% / 80%);
-  
+  color: hsl(0deg 0% 13% / 80%);
 `;
 
 const Button = styled.button`
@@ -188,12 +203,13 @@ const Section = styled.div`
   font-size: 18px;
 `;
 
-const Input= styled.input`
-width: 200px;
-height: 25px;
-border-radius: 8px;
-`
-const TextError= styled.p`
-font-weight: bold;
-color:red;
-`
+const Input = styled.input`
+  width: 200px;
+  height: 25px;
+  border-radius: 8px;
+  margin-bottom: 1.2rem;
+`;
+const TextError = styled.p`
+  font-weight: bold;
+  color: red;
+`;
