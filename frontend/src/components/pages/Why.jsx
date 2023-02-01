fdf26d863abd92c0d1f0cd420bf1f74020850b6e
Tara78 <70331037+Tara78@users.noreply.github.com>
2023-02-01 16:32:20 +0100

    whyPage.Translation

--- frontend/src/components/pages/Why.jsx
+++ frontend/src/components/pages/Why.jsx
@@ -3,90 +3,81 @@ import styled from "styled-components";
 import imgGustald from "./../../img/Gustald-tree.jpg";
 import imgWhy from "./../../img/consultimg.png";
 import { Hero, SectionBackgroundImg, Page } from "../Base";
-import {Title, SectionSubTitle, WhyGustaldText, BoldText} from "./Why.styles";
+import {
+  Title,
+  SubTitle,
+  SectionSubTitle,
+  WhyGustaldText,
+  BoldText,
+  Point,
+  TitleHow,
+  SubTitleHow,
+  Paragrah1,
+  Paragrah2,
+  Paragrah3,
+} from "./Why.styles";
 import { useTranslation } from "react-i18next";
 
 const WhyPage = () => {
-   const { t } = useTranslation();
+  const { t } = useTranslation();
   return (
     <>
       <Hero>
         <Section1BackgroundImg></Section1BackgroundImg>
       </Hero>
       <Page>
-        {/* {t("tarif.Title")} */}
-        <Title>Pourquoi choisir la Gestalt-thérapie ?</Title>
+        <Title>{t("why.Title")}</Title>
         <SectionSubTitle>
-          La Gestalt-thérapie s'adresse à tous, adultes, adolescents ou enfants
-          pour traiter:
-          <p>
-            <BoldText>• des problèmes relationnels: </BoldText>
-            timidité, sentiment d’exclusion.
-          </p>
-          <p>
-            <BoldText>• des problèmes affectifs: </BoldText>
-            problème de couple, séparation difficile, deuil.
-          </p>
-          <p>
-            <BoldText>• des troubles de conduites alimentaires.</BoldText>
-          </p>
-          <p>
-            <BoldText>• des troubles psychiques: </BoldText>
-            anxiété, dépression, troubles bipolaires, troubles psychosomatiques.
-          </p>
-          <p>
-            <BoldText>• des troubles émotionnels: </BoldText>stress, angoisse.
-          </p>
-          <p>
-            <BoldText>
-              • des situations issues du monde professionnel:{" "}
-            </BoldText>
-            harassement, chômage, sens du travail,…
-          </p>
+          <SubTitle> {t("why.SubTitle")}</SubTitle>
+          <Point>
+            <BoldText> {t("why.BoldText1")} </BoldText>
+            {t("why.Point1")}
+          </Point>
+          <Point>
+            <BoldText>{t("why.BoldText2")}</BoldText>
+            {t("why.Point2")}
+          </Point>
+          <Point>
+            <BoldText>{t("why.BoldText3")}</BoldText>
+          </Point>
+          <Point>
+            <BoldText>{t("why.BoldText4")}</BoldText>
+            {t("why.Point4")}
+          </Point>
+          <Point>
+            <BoldText>{t("why.BoldText5")} </BoldText>
+            {t("why.Point5")}
+          </Point>
+          <Point>
+            <BoldText>{t("why.BoldText6")}</BoldText>
+            {t("why.Point6")}
+          </Point>
         </SectionSubTitle>
         <WhyGustaldText>
-          A partir de la problématique de la personne qui vient consulter,
-          l’idée poursuivie est non seulement de lui permettre de mieux faire
-          face à celle-ci mais également de lui permettre de reprendre sa vie en
-          main, s'affirmer, s'épanouir, trouver son potentiel dans ce que l’on
-          appelle le
-          <BoldText> développement personnel.</BoldText>
+          {t("why.WhyGustaldText")}
+          <BoldText> {t("why.BoldTextWhy")}</BoldText>
         </WhyGustaldText>
 
         <Hero>
           <Section2BackgroundImg></Section2BackgroundImg>
         </Hero>
         <SectionSubTitle>
-          <h1>Comment se déroule une séance de Gestalt-thérapie?</h1>
-          <SectionSubTitle>
-            La Gestalt-thérapie se pratique donc de manière individuelle ou en
-            groupe, c'est le thérapeute qui décide de la forme de la thérapie la
-            mieux adaptée à la situation du patient. Elle fait partie des
-            thérapies à moyen ou long terme puisqu'en général elle nécessite une
-            séance tous les 15 jours sur une durée généralement comprise entre 6
-            et 12 mois. Après quelques mois, la personne qui consulte commence à
-            ressentir un mieux dans ses relations et dans sa vie quotidienne et
-            il est alors important qu’elle s’assure que ses acquis sont
-            consolidés de façon pérenne.
-          </SectionSubTitle>
-          <h3>Une séance de cette thérapie s'organise sur 3 axes:</h3>
-          <p>
-            <BoldText> • La verbalisation: </BoldText>
-            la personne évoque sa situation, son problème, le thérapeute l'aide
-            à rester attentive à ses gestes et à ses émotions
-          </p>
+          <TitleHow>{t("how.TitleHow")}</TitleHow>
+          <SubTitleHow>{t("how.SubTitleHow")}</SubTitleHow>
+          <h3>{t("how.h3")}</h3>
+          <Paragrah1>
+            <BoldText> {t("how.BoldText1")}</BoldText>
+            {t("how.Paragrah1")}
+          </Paragrah1>
 
-          <p>
-            <BoldText> • L’expérimentation: </BoldText>
-            jeux de rôle ou créativité artistique pour noter la façon dont le
-            patient interagit avec lui-même, les autres ou les objets.
-          </p>
-          <p>
-            <BoldText>• Le changement: </BoldText>
-            en fonction de la deuxième phase, le thérapeute recherche les pistes
-            pour mener à un changement à amorcer afin d'équilibrer les contacts
-            et les relations entre la personne et son environnement
-          </p>
+          <Paragrah2>
+            <BoldText>{t("how.BoldText2")} </BoldText>
+            {t("how.Paragrah2")}
+          </Paragrah2>
+          <Paragrah3>
+            <BoldText>{t("how.BoldText3")}</BoldText>
+            {t("how.Paragrah3")}
+          </Paragrah3>
         </SectionSubTitle>
       </Page>
     </>
@@ -96,7 +87,6 @@ const WhyPage = () => {
 const Section1BackgroundImg = styled(SectionBackgroundImg)`
   background-image: url(${imgGustald});
 `;
-
 
 const Section2BackgroundImg = styled(SectionBackgroundImg)`
   background-image: url(${imgWhy});
--- frontend/src/components/pages/Why.styles.jsx
+++ frontend/src/components/pages/Why.styles.jsx
@@ -1,11 +1,39 @@
 import styled from "styled-components";
 
-export const Tilte= styled.h1`
-`
+export const Title = styled.h1`
+  font-family: "Poppins", sans-serif;
+  color: #343131;
+  line-height: 29px;
+  font-size: 22px;
+
+  @media (max-width: 375px) {
+    width: 90%;
+    font-size: 15px;
+  }
+`;
 
 export const SectionSubTitle = styled.div`
   line-height: 39px;
   color: #343131;
+`;
+
+export const SubTitle = styled.p`
+  color: #343131;
+  line-height: 29px;
+  font-size: 22px;
+  @media (max-width: 375px) {
+    width: 90%;
+    font-size: 15px;
+  }
+`;
+
+export const Point = styled.p`
+  font-size: 20px;
+  line-height: 39px;
+`;
+
+export const BoldText = styled.span`
+  font-weight: bold;
 `;
 
 export const WhyGustaldText = styled.p`
@@ -14,6 +42,36 @@ export const WhyGustaldText = styled.p`
   line-height: 39px;
 `;
 
-export const BoldText = styled.span`
-  font-weight: bold;
+export const TitleHow = styled.h1`
+  /* font-family: "Poppins", sans-serif; */
+  color: #343131;
+  line-height: 29px;
+  margin-top: 2rem;
+  font-size: 18px;
+  @media (max-width: 375px) {
+    width: 90%;
+    font-size: 15px;
+  }
+`;
+export const SubTitleHow = styled.p`
+   color: #343131;
+  line-height: 29px;
+  font-size: 20px;
+  @media (max-width: 375px) {
+    width: 90%;
+    font-size: 15px;
+  }
+  
+`;
+export const Paragrah1 = styled.p`
+  font-size: 20px;
+  line-height: 39px;
+`;
+export const Paragrah2 = styled.p`
+  font-size: 20px;
+  line-height: 39px;
+`;
+export const Paragrah3 = styled.p`
+  font-size: 20px;
+  line-height: 39px;
 `;
