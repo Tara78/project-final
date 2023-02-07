15e8ee1b5d7e59bfac8d54e25142b0010f030811
Tara78 <70331037+Tara78@users.noreply.github.com>
2023-02-07 11:57:41 +0100

    url backend

--- frontend/src/utils/utils.jsx
+++ frontend/src/utils/utils.jsx
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.API_URL || "http://localhost:8080";
+const BASE_URL =
+  process.env.API_URL || "https://project-final-tpiwomq62a-lz.a.run.app";
 
 
 // Slug is a suffix of the request and it comes after base url request/
