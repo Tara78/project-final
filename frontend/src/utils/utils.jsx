923834db589559d979875a71965f54b71e79882e
Tara Hassani <70331037+Tara78@users.noreply.github.com>
2023-02-23 16:15:52 +0100

    update url

--- frontend/src/utils/utils.jsx
+++ frontend/src/utils/utils.jsx
@@ -1,12 +1,8 @@
 const BASE_URL =
-<<<<<<< HEAD
+
   process.env.API_URL || "http://localhost:8080";
 /*   "https://project-final-tpiwomq62a-lz.a.run.app";  */
 
-=======
-  // process.env.API_URL || "https://project-final-tpiwomq62a-lz.a.run.app";
-  process.env.API_URL || "http://localhost:8080";
->>>>>>> c31e729bbd15f8aa150868f2222157352f825a48
 
 // Slug is a suffix of the request and it comes after base url request/
 // Slug is sort of the end point of the reuest
